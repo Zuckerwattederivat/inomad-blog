3848304f2c6af352d4a79c9d2bc391e392071849
Brian Boehm <brianboehm.web@gmail.com>
2020-08-17 02:05:35 +0200

    Removed db creds from config

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,8 @@
 <?php
   // DB Parameters
-  define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASS', 'root');
+  define('DB_HOST', '');
+  define('DB_USER', '');
+  define('DB_PASS', '');
   define('DB_NAME', 'iNomad');
 
   // App Root
