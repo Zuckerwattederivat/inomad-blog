43d9dfad3264327e206d23034ed58e6e505a34a7
Brian Boehm <brianboehm.web@gmail.com>
2020-09-02 17:49:00 +0200

    Fixed bug in categories and search, where username didn't link to profile page

--- app/views/posts/category.php
+++ app/views/posts/category.php
@@ -23,7 +23,7 @@
               <div class="card-body">
                 <h3 class="name"><a class="text-dark" href="<?php echo URLROOT."/posts/show/".$post->post_id; ?>"><?php echo $post->post_title; ?></a></h3>
                 <p class="lead">
-                  by <a class="text-dark" href="#"><?php echo $post->user_alias; ?></a>
+                  by <a class="text-dark" href="<?php echo URLROOT."/users/posts/".$post->post_user_id; ?>"><?php echo $post->user_alias; ?></a>
                 </p>
                 <p>
                   <span><i class="fa fa-calendar-times-o" aria-hidden="true"></i></span>
